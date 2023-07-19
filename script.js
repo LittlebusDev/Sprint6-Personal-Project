67fc808cc16c9b8e3c1ed4606e7d72aff23e3001
Tori Stevens <tori.stevens13@gmail.com>
2023-07-19 21:02:37 +1200

    add initial design and set-up

---
+++ script.js
@@ -0,0 +1,30 @@
+const patterns = [
+  {
+    name: 'Pattern One',
+    needleSize: '2',
+    yarnWeight: '2',
+    difficulty: 'Advanced',
+    imgSrc: './images/pattern-one.png',
+  },
+  {
+    name: 'Pattern Two',
+    needleSize: '6',
+    yarnWeight: '4',
+    difficulty: 'Beginner',
+    imgSrc: './images/pattern-two.png',
+  },
+  {
+    name: 'Pattern Three',
+    needleSize: '4',
+    yarnWeight: '3',
+    difficulty: 'beginner-Intermediate',
+    imgSrc: './images/pattern-three.png',
+  },
+  {
+    name: 'Pattern Four',
+    needleSize: '1',
+    yarnWeight: '0',
+    difficulty: 'Intermediate-Advanced',
+    imgSrc: './images/pattern-four.png',
+  },
+]
